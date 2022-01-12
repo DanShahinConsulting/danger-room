f350849bba9836d8755167149f7848dfa90e5375
Dan Shahin <dan@danshahin.com>
2022-01-11 21:20:17 -0800

    save cam position

--- src/js/webcamgl.js
+++ src/js/webcamgl.js
@@ -79,29 +79,22 @@ function onWindowResize() {
     render()
 }
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
-let camdata = JSON.parse(localStorage.getItem('camdata') );
-if(camdata){
-    console.log({camdata})
-    //controls.target.set(camdata.x, camdata.y, camdata.z);
-    
-    //controls.update();
-    // camera.position.x = camdata.position.x
-    // camera.position.y = camdata.position.y
-    // camera.position.z = camdata.position.z
 
-    // camera.rotation.x = camdata.rotation.x
-    // camera.rotation.y = camdata.rotation.y
-    // camera.rotation.z = camdata.rotation.z
-    // console.log({cameraPosition})
-    // camera.position.set(cameraPosition);
-}
+
+const savedCamera = JSON.parse( localStorage.getItem( 'savedCamera' ) );
+  if( savedCamera ){
+    camera.position.copy( savedCamera.cameraPosition );
+    controls.target.copy( savedCamera.targetPosition );
+  }
 
 
 controls.addEventListener( 'change', function(evt, foo){
     console.log(evt, controls, camera.position)
-    localStorage.setItem('camdata', JSON.stringify(camera.position) );
-    localStorage.setItem('camrotation', JSON.stringify(camera.rotation) );
-    
+    localStorage.setItem('userScene', JSON.stringify(scene.toJSON()) );
+    localStorage.savedCamera = JSON.stringify({
+        cameraPosition: camera.position,
+        targetPosition: controls.target
+      });
 } );
 
 
@@ -321,6 +314,10 @@ function bindKeys(){
         }
         //console.log(plane)
         localStorage.setItem('userScene', JSON.stringify(scene.toJSON()) );
+        localStorage.savedCamera = JSON.stringify({
+            cameraPosition: camera.position,
+            targetPosition: controls.target
+          });
         //localStorage.setItem('userCamera', JSON.stringify(plane) );
     });
 }
